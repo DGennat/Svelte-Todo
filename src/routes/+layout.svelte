621bd104ac56fac1c452f5e85e525b2bef545507
Daniel Gennat <d.gennat@neusta.de>
2023-06-23 16:28:04 +0200

    add shell to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,20 @@
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<nav>
-	<a href="/">home</a>
-	<a href="/login">login</a>
-	<a href="/todo">todo</a>
-</nav>
+<AppShell>
+	<svelte:fragment slot="header">
+		<h1 class="h1">Svelte Todo</h1>
+	</svelte:fragment>
 
-<slot />
+	<slot />
+	<svelte:fragment slot="footer">
+		<nav>
+			<a class="anchor" href="/">home</a>
+			<a class="anchor" href="/login">login</a>
+			<a class="anchor" href="/todo">todo</a>
+		</nav>
+	</svelte:fragment>
+</AppShell>
--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,1 +1,1 @@
-<h1>Todo</h1>
+<h2 class="h2">Todo</h2>
