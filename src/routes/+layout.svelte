1fce2dcaa9ac1400f29186e9118001f73e8313e8
Daniel Gennat <d.gennat@neusta.de>
2023-06-23 16:12:42 +0200

    install skeleton and tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+</script>
+
 <nav>
 	<a href="/">home</a>
 	<a href="/login">login</a>
